e9eb09d6bcb88f44502b500f2869343e36e1a4c1
mintusmaximus <jheikkinen109@gmail.com>
2025-08-29 18:49:57 +0300

    todo part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,66 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // a single task is a string, default empty
+  const [taskField, setTaskField] = useState('')
+  // tasks is array of strings, default empty
+  const [tasksList, setTasksList] = useState([])
+  
+  // gets called on enter press on 'Add new task' input field
+  const addTask = () => {
+    // immutable spread operator, add taskField to taskList (useState variable), creating a new array
+    setTasksList([...tasksList,taskField]) 
+    setTaskField('') // reset input field
+  }
+
+  const deleteTask = (deleted) => {
+    // create new array of elements that dont have removed elements
+    // use filter to remove item via function parameter
+    const withoutRemoved = tasksList.filter(item => item!==deleted) // if item currently being iterated isnt the same as parameter, add to new list
+    setTasksList(withoutRemoved) // set new list as tasksList once deleted item has been removed.
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="container">
+        <h3>Todos</h3>
+        <form>
+          <input 
+            placeholder='Add new task'
+            value={taskField} // input field gets its value constantly from usestate
+            onChange={e => setTaskField(e.target.value)} // when change to input field happens, get value and set to task field state variable
+            onKeyDown={e => { // when any key is pressed
+              if (e.key === 'Enter') { // check if its enter
+                e.preventDefault() // dont refresh
+                addTask() // add to taskList
+              }
+            }}
+          />
+        </form>
+        {/* create unordered list */}
+        <ul>
+          {/* create inline js function */}
+          {
+            // iterate through array items with map
+            // (parameter) => (return value)
+            tasksList.map(item => (
+              // create new list item every time
+              <li>
+                {/* add item inside list */}
+                {item}
+                {/* also add a button beside each task */}
+                <button
+                // give button classname
+                  className='delete-button'
+                  // when clicking this button, call delete with task inside <li>
+                  onClick={()=>deleteTask(item)}>
+                  Delete task
+                </button>
+              </li>
+            ))
+          }
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
